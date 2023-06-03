1fcf9bdf05af3a2e6e8b59cdabe2384e6944c2ab
Sava <87912493+savaonepunch@users.noreply.github.com>
2023-06-03 10:31:39 +0300

    Update FindDevelopersSection.vue

--- src/components/hire-developers/FindDevelopersSection.vue
+++ src/components/hire-developers/FindDevelopersSection.vue
@@ -5,58 +5,58 @@
             <p>From GraphQL to AWS or Laravel. Our job seekers are not limited to just Vue.js knowledge nor a frontend only stack.</p>
             <div class="cards">
                 <div style="background-color: #00DC81;" class="card">
-                    <img src="../../assets/white-logos/nuxt-icon-white.svg" alt="">
+                    <img src="../../assets/white-logos/nuxt-icon-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #1497F6;" class="card">
-                    <img src="../../assets/white-logos/vuetify-icon-white.svg" alt="">
+                    <img src="../../assets/white-logos/vuetify-icon-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #FF2C1F;" class="card">
-                    <img src="../../assets/white-logos/laravel-icon-white.svg" alt="">
+                    <img src="../../assets/white-logos/laravel-icon-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #1C2024;" class="card">
-                    <img src="../../assets/white-logos/wordpress-icon-white.svg" alt="">
+                    <img src="../../assets/white-logos/wordpress-icon-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #007ACB;" class="card">
-                    <img src="../../assets/white-logos/typescript-icon.svg" alt="">
+                    <img src="../../assets/white-logos/typescript-icon.svg?url" alt="">
                 </div>
                 <div style="background-color: #8CC74A;" class="card">
-                    <img src="../../assets/white-logos/nodejs-icon-white.svg" alt="">
+                    <img src="../../assets/white-logos/nodejs-icon-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #61D9FB;" class="card">
-                    <img src="../../assets/white-logos/react-icon-white.svg" alt="">
+                    <img src="../../assets/white-logos/react-icon-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #FF3E00;" class="card">
-                    <img src="../../assets/white-logos/svelte-icon.svg" alt="">
+                    <img src="../../assets/white-logos/svelte-icon.svg?url" alt="">
                 </div>
                 <div style="background-color: #37BCF8;" class="card">
-                    <img src="../../assets/white-logos/tailwind-icon-white.svg" alt="">
+                    <img src="../../assets/white-logos/tailwind-icon-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #E0234D;" class="card">
-                    <img src="../../assets/white-logos/nestjs-icon-white.svg" alt="">
+                    <img src="../../assets/white-logos/nestjs-icon-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #4DAAD5;" class="card">
-                    <img src="../../assets/white-logos/go-logo-white.svg" alt="">
+                    <img src="../../assets/white-logos/go-logo-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #000000;" class="card">
-                    <img src="../../assets/white-logos/rust-logo-white.svg" alt="">
+                    <img src="../../assets/white-logos/rust-logo-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #FF9900;" class="card">
-                    <img src="../../assets/white-logos/aws-logo-white.svg" alt="">
+                    <img src="../../assets/white-logos/aws-logo-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #EFDB4F;" class="card">
-                    <img src="../../assets/white-logos/javascript-logo.svg" alt="">
+                    <img src="../../assets/white-logos/javascript-logo.svg?url" alt="">
                 </div>
                 <div style="background-color: #E535AB;" class="card">
-                    <img src="../../assets/white-logos/graphql-icon-white.svg" alt="">
+                    <img src="../../assets/white-logos/graphql-icon-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #8993BE;" class="card">
-                    <img src="../../assets/white-logos/php-logo-white.svg" alt="">
+                    <img src="../../assets/white-logos/php-logo-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #DD4B36;" class="card">
-                    <img src="../../assets/white-logos/git-icon-white.svg" alt="">
+                    <img src="../../assets/white-logos/git-icon-white.svg?url" alt="">
                 </div>
                 <div style="background-color: #CC0100;" class="card">
-                    <img src="../../assets/white-logos/rails-logo-white.svg" alt="">
+                    <img src="../../assets/white-logos/rails-logo-white.svg?url" alt="">
                 </div>
                 
             </div>
